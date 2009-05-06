c36b4184242ea333d15968f9971188f178ceb4aa
Gerry Gleason <gerry@gerry-gleasons-computer.local>
2009-05-06 14:53:09 -0500

    More playing with air deploy mode

--- air/clean.sh
+++ air/clean.sh
@@ -1,3 +1,3 @@
-BINLIST="air/MyOpenLaszloAirApp.swf air/OpenLaszloAir.air air/flx/MyOpenLaszloAirApp.swf air/flx/my-openlaszlo-air-app.lzx.swf7.swf air/openlaszlocert.pfx"
+BINLIST="air/MyOpenLaszloAirApp.swf air/OpenLaszloAir.air air/flx/MyOpenLaszloAirApp.swf air/flx/cardtest.swf7.swf air/openlaszlocert.pfx"
 rm $BINLIST
 rm -r air/work
